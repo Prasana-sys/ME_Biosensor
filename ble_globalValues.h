2bb4690c068590473359904f7741022a2633c016
Prasana Ganesh <77466443+Prasana-sys@users.noreply.github.com>
2025-03-28 11:31:33 -0400

    cont: Cleanup

--- ble_globalValues.h
+++ ble_globalValues.h
@@ -10,8 +10,8 @@ extern volatile uint8_t btn_state;
 
 const uint8_t advertised_name[] = "ME Biosensor";
 extern uint8_t connection_handle;
-extern uint8_t ringdownData_characteristic_handle;
-extern uint8_t ringdownParameters_characteristic_handle;
+extern uint16_t ringdownData_characteristic_handle;
+extern uint16_t ringdownParameters_characteristic_handle;
 
 extern bool clientReadyForNextIndication;
 extern uint32_t indicationIndex;  // Number of steps
